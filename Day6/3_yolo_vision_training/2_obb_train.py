b382a3e0078b268549cc88d8f0381db88a93e4b0
dhgustjr8871 <dhgustjr8871@snu.ac.kr>
2025-08-06 05:05:46 +0900

    Day 6 finalized

---
+++ Day6/3_yolo_vision_training/2_obb_train.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+	# Load a pretrained YOLO11n model
+	model = YOLO("yolo11n-obb.yaml").load("yolo11n-obb.pt")
+
+	results = model.train(data=r"datasets\obb\data.yaml", epochs=250, imgsz=1024, device=0)
+	metrics = model.val()
+
+	results=model(source="eval_image.png",save=True)
+	results[0].show()  # Display results
+
+	path = model.export(format="onnx")  # Returns the path to the exported model
---
+++ Day6_old/3_yolo_vision_training/README.md
@@ -0,0 +1,7 @@
+##yolo
+
+torch, torchvision, numpy -> 버전 충돌 가능
+
+pip install torch==2.2.2+cu118
+pip install torchvision==0.17.2+cu118 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu11
+pip install numpy==1.26.4
