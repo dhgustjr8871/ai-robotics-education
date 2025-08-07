04bdb8d3e8802b073e33b987c6049255019de76b
dhgustjr8871 <dhgustjr8871@snu.ac.kr>
2025-08-07 15:46:34 +0900

    readmeme

---
+++ Day6_old/1_collect_data/set_position.py
@@ -0,0 +1,103 @@
+# 로봇이 현재 pose에서 상대적으로 얼마나 더 이동할지를 명령하는 코드
+
+import asyncio
+import socket
+import struct
+
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+def print2(str, color=Color.YELLOW):
+    print(color, str, Color.END)
+
+PORT_PRIMARY_CLIENT = 30001
+PORT_SECONDARY_CLIENT = 30002
+
+server_ip = "192.168.1.5"
+robot_ip = "192.168.1.4"
+script_path = "scripts/socket_set_position.script"
+
+async def handle_client(reader, writer):
+    addr = writer.get_extra_info('peername')
+    print(f"Connected by {addr}")
+    
+    try:
+        while True:
+            data = await reader.read(1024)
+            if not data:
+                break
+            message = data.decode('utf-8').rstrip()  # Remove trailing newline
+
+            print(f"Received from {addr}: {message}")
+
+            if message == "req_data":
+                print("Received data request")
+                p_rel = [0.0, 0.0, 0.0, -1.0, 0.0, 0.0]
+                float_string = "({})\n".format(','.join(map(str, p_rel)))
+                writer.write(float_string.encode())
+                await writer.drain()
+            
+    except asyncio.CancelledError:
+        pass
+    except ConnectionResetError:
+        print(f"Connection with {addr} reset")
+    # except Exception as e:
+    #     print("Error:", e)
+    finally:
+        print(f"Connection with {addr} closed")
+        writer.close()
+        # await writer.wait_closed()
+
+
+async def handle_pos_data(reader):
+    integers_data = []
+    # Receive 24 bytes (6 integers = 6 * 4 bytes = 24 bytes) 
+    data = await reader.readexactly(24)
+    # Unpack the 6 short integers from the received data
+    print("position data:", data)
+    integers_data = struct.unpack('>iiiiii', data)
+    actual_pos_data = [x/10000 for x in integers_data]
+
+    return actual_pos_data
+
+async def main(host='0.0.0.0', port=12345):
+    server = await asyncio.start_server(handle_client, host, port)
+    addr = server.sockets[0].getsockname()
+    print(f"Server listening on {addr}")
+    print("Sending script to the robot...")
+
+    await asyncio.sleep(0.1)
+    sendScriptFile(robot_ip, script_path, PORT_PRIMARY_CLIENT)
+
+    async with server:
+        await server.serve_forever()
+
+def getScriptFromPath(script_path):
+    with open(script_path, 'r') as file:
+        script = file.read()
+    return script
+
+def sendScript(robot_url, script, port=PORT_PRIMARY_CLIENT):
+    socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketClient.connect((robot_url, port))
+    socketClient.send((script + "\n").encode())
+    socketClient.close()
+
+def sendScriptFile(robot_url, script_path, port=PORT_PRIMARY_CLIENT):
+    script = getScriptFromPath(script_path)
+    sendScript(robot_url, script, port)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main(host=server_ip))
+    except KeyboardInterrupt:
+        print("\nServer is shutting down...")
