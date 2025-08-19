39a265eee189d7903cbae8406a9da6424d25f6cb
dhgustjr8871 <dhgustjr8871@snu.ac.kr>
2025-08-19 20:55:12 +0900

    Update 2_obb_train.py

--- Day6/3_yolo_vision_training/2_obb_train.py
+++ Day6/3_yolo_vision_training/2_obb_train.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 	# Load a pretrained YOLO11n model
 	model = YOLO("yolo11n-obb.yaml").load("weights/obb/best.pt")
 
-	results = model.train(data="datasets/obb/data.yaml", epochs=1000, imgsz=1024, device=0)
+	results = model.train(data="datasets/obb/data.yaml", epochs=100, imgsz=1024, device=0)
 	metrics = model.val()
 
 	results=model(source="eval_image.png",save=True)
